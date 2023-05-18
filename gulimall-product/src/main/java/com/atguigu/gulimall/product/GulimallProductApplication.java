6b8a0d788f0ec03c7e56684311ca6794ad08c313
lei <1404221253@qq.com>
2023-05-18 21:24:32 +0800

    逻辑删除

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1、整合Mybatis-Plus
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *   2、配置Mybatis-Plus
  *   1)、配置数据源
  */
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -73,11 +73,13 @@ public class CategoryController {
 
     /**
      * 删除
+     * @RequestBody:获取请求体，必须发送POST请求
      */
     @RequestMapping("/delete")
     public R delete(@RequestBody Long[] catIds) {
-        categoryService.removeByIds(Arrays.asList(catIds));
-
+        //1、检查当前删除的菜单，是否被别的地方引用
+//        categoryService.removeByIds(Arrays.asList(catIds));
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
         return R.ok();
     }
 
